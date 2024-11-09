a3697aca0e4fcded2680bd2d25fbee355b6f67d4
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2024-11-08 21:30:16 -0500

    add login and logout feature

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let token = null;
+
 (() => {
   //init function
   //generate a loader from a template
@@ -6,7 +8,24 @@
     let spinner = temp.content.cloneNode(true); // documentFragment
     return spinner.firstElementChild; //html inside the fragment
   })();
-  fetchImage(honeycomb);
+  document.body.addEventListener('click', () => {
+    if (token) {
+      //logged in
+      console.log('fetch');
+      fetchImage(honeycomb);
+    } else {
+      //not logged in yet
+      login();
+    }
+  });
+
+  document.querySelector('#logout').addEventListener('click', (ev) => {
+    ev.preventDefault();
+    ev.stopPropagation(); //stop bubbling from triggering login
+    token = null;
+    document.querySelector('header').classList.remove('logged-in');
+  });
+
   document.addEventListener('fullscreenchange', (ev) => {
     console.log(ev);
   });
@@ -24,9 +43,69 @@
   });
 })();
 
+function login() {
+  let req = new Request('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({
+      username: 'emilys',
+      password: 'emilyspass',
+    }),
+  });
+  fetch(req)
+    .then((response) => {
+      if (!response.ok) throw new Error('Invalid login attempt');
+      token = null;
+      return response.json();
+    })
+    .then((data) => {
+      token = data.accessToken;
+      document.querySelector('header').classList.add('logged-in');
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+}
+
 function fetchImage(spinner) {
   //use fetch to get an image
   //if a spinner is provided show before fetch and remove after
+  const main = document.querySelector('main');
+  main.classList.add('loading'); // in the css
+  main.append(spinner);
+
+  //prepare the request
+  let width = Math.floor(Math.random() * 2000) + 1000;
+  let height = Math.floor(Math.random() * 1000) + 1000;
+  const url = `https://dummyjson.com/auth/image/${width}x${height}`;
+  const request = new Request(url, {
+    method: 'GET',
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+
+  fetch(request)
+    .then((response) => {
+      console.log(response.status);
+      if (!response.ok) throw new Error('Bad dog');
+      return response.blob();
+    })
+    .then((blob) => {
+      let img = document.querySelector('main img');
+      const src = URL.createObjectURL(blob);
+      console.log(src);
+      img.src = src;
+      main.classList.remove('loading');
+      main.removeChild(spinner);
+    })
+    .catch((err) => {
+      console.warn(err.message);
+      main.classList.remove('loading');
+      main.removeChild(spinner);
+      main.querySelector('img').src = 'img/placeholder.png';
+      main.innerHTML += 'Tell your developer to think harder.';
+    });
 }
 
 function storeData(key, value) {
